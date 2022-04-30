2a6082238bd5b104300690239e794f0b4c2b8641
proton2456 <104689313+proton2456@users.noreply.github.com>
2022-04-30 22:10:19 +0530

    Created gradesystem.c
    Its gives us output of grades.

---
+++ gradesystem.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    
+int num;
+printf("Enter your mark ");
+scanf("%d",&num);
+printf(" You entered %d", num);
+
+	if(num >= 85){
+	printf(" You got A grade"); 
+		}
+	else if ( num >= 70){ 
+		printf(" You got B grade");
+		}
+	else if ( num >=55){
+		printf(" You got C grade");
+		}
+	else if ( num >=40){
+		printf(" You got D grade");
+		}
+	else if ( num < 40){
+		printf(" You got F grade");
+		}
+return 0;
+}
